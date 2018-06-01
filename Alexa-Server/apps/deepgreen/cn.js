40fc389f483bad97bf28d97ebd2658a315114e50
unknown <julien_max.garb@fh-bielefeld.de>
2018-06-01 09:40:51 +0200

    fixed answers to newGame

--- Alexa-Server/apps/deepgreen/cn.js
+++ Alexa-Server/apps/deepgreen/cn.js
@@ -3,17 +3,20 @@ exports.newGame = function () {
     console.log("hi");
     const app = require('express')();
     const http = require('http').Server(app);
-    let socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:5000');
+    let socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999');
     socket.on('connect', function() {
         console.log("emitted");
         socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false });
     });
 
-    socket.on('invitation', function() {
-        console.log("invited");
+    socket.on('receive', function() {
+        console.log("received");
+    });
+    socket.on('reject', function() {
+        console.log("rejected");
     });
 
-    http.listen(5000);
+
 
 
 }
