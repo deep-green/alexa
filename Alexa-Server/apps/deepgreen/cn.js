49a2116c54292f8d8e339926678313e2e6f9bb87
unknown <julien_max.garb@fh-bielefeld.de>
2018-06-01 11:22:04 +0200

    removed unneecessary dependencies

--- Alexa-Server/apps/deepgreen/cn.js
+++ Alexa-Server/apps/deepgreen/cn.js
@@ -1,13 +1,9 @@
+const socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999/deep-green');
 
 exports.newGame = function () {
-    console.log("hi");
-    const app = require('express')();
-    const http = require('http').Server(app);
-    let socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999');
-    socket.on('connect', function() {
-        console.log("emitted");
-        socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false });
-    });
+
+    socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" },
+                console.log("newGame emitted"));
 
     socket.on('receive', function() {
         console.log("received");
@@ -15,8 +11,4 @@ exports.newGame = function () {
     socket.on('reject', function() {
         console.log("rejected");
     });
-
-
-
-
 }
