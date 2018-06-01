a4921e1d597cf0a145b9965e482c743ba34bb5e6
unknown <julien_max.garb@fh-bielefeld.de>
2018-06-01 09:44:19 +0200

    fixed comma

--- Alexa-Server/server.js
+++ Alexa-Server/server.js
@@ -4,11 +4,11 @@ var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
 	httpsEnabled: true,
-	port: 6500
+	port: 6500,
 	httpsPort: 6501,
 	privateKey: 'private-key.pem',
 	certificate: 'cert.cer'
- 
+
 } );
 
 server.start()
