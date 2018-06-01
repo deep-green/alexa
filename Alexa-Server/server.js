6907dd5b6c446c00bedddfe5135211c2c93f9264
unknown <julien_max.garb@fh-bielefeld.de>
2018-06-01 09:42:43 +0200

    test https

--- Alexa-Server/server.js
+++ Alexa-Server/server.js
@@ -3,13 +3,12 @@
 var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
-	httpsEnabled: false,
+	httpsEnabled: true,
 	port: 6500
-	/*httpsPort: 55001,
- httpsEnabled: true,
- privateKey: 'private-key.pem',
- certificate: 'cert.cer'
- */
+	httpsPort: 6501,
+	privateKey: 'private-key.pem',
+	certificate: 'cert.cer'
+ 
 } );
 
 server.start()
