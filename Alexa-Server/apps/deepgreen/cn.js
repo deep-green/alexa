c7304ccb67a9c05413421a1e9a25a57cd307a923
unknown <julien_max.garb@fh-bielefeld.de>
2018-06-01 09:25:36 +0200

    port aenderung

--- Alexa-Server/apps/deepgreen/cn.js
+++ Alexa-Server/apps/deepgreen/cn.js
@@ -1,3 +1,19 @@
+
 exports.newGame = function () {
     console.log("hi");
-};
+    const app = require('express')();
+    const http = require('http').Server(app);
+    let socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:5000');
+    socket.on('connect', function() {
+        console.log("emitted");
+        socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false });
+    });
+
+    socket.on('invitation', function() {
+        console.log("invited");
+    });
+
+    http.listen(5000);
+
+
+}
--- Alexa-Server/server.js
+++ Alexa-Server/server.js
@@ -4,7 +4,7 @@ var AlexaAppServer = require( 'alexa-app-server' );
 
 var server = new AlexaAppServer( {
 	httpsEnabled: false,
-	httpPort: 6500
+	port: 6500
 	/*httpsPort: 55001,
  httpsEnabled: true,
  privateKey: 'private-key.pem',
