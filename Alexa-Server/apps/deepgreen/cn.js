462076d6b2e423edb7185d32ffe957c9ff6effa9
Julien <julien_max.garb@fh-bielefeld.de>
2018-06-14 16:45:46 +0200

    new ssl certificat

--- Alexa-Server/apps/deepgreen/cn.js
+++ Alexa-Server/apps/deepgreen/cn.js
@@ -1,14 +1,14 @@
-const socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999/deep-green');
+const socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999');
 
 exports.newGame = function () {
 
     socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" },
                 console.log("newGame emitted"));
 
-    socket.on('receive', function() {
-        console.log("received");
+    socket.once('receive', function(msg) {
+        console.log(msg);
     });
-    socket.on('reject', function() {
+    socket.once('reject', function() {
         console.log("rejected");
     });
 }
--- Alexa-Server/server.js
+++ Alexa-Server/server.js
@@ -5,7 +5,10 @@ var AlexaAppServer = require( 'alexa-app-server' );
 var server = new AlexaAppServer( {
 	httpsEnabled: true,
 	port: 6500,
+	log: true,
 	httpsPort: 6501,
+	verify:true,
+	debug: false,
 	privateKey: 'private-key.pem',
 	certificate: 'cert.cer'
 
