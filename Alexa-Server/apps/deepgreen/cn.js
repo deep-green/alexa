49661388f30f858a69e6948c15bb663db5e8c27d
Julien <julien_max.garb@fh-bielefeld.de>
2018-06-15 14:17:50 +0200

    Variablen reihenfolge gefixed

--- Alexa-Server/apps/deepgreen/cn.js
+++ Alexa-Server/apps/deepgreen/cn.js
@@ -1,6 +1,6 @@
 const socket = require('socket.io-client')('http://ec2-54-93-171-91.eu-central-1.compute.amazonaws.com:4999');
 
-exports.newGame = function (sendResponse,response) {
+exports.newGame = function (response, sendResponse) {
 
     socket.emit('newGame', { FEN: 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1', ID_enemy: 'maxmustermann', color: false, token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" },
                 console.log("newGame emitted"));
